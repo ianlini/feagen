7487f9a4367e5372ce486fd666e60c25add8d523
Ian Lin <andylin514@gmail.com>
2016-12-04 23:14:42 +0800

    remove redundant code

---
+++ feagen/decorators.py
@@ -0,0 +1,50 @@
+def require(data_keys):
+    if isinstance(data_keys, str):
+        data_keys = (data_keys,)
+
+    def require_decorator(func):
+        # pylint: disable=protected-access
+        if not hasattr(func, '_feagen_require'):
+            func._feagen_require = []
+        func._feagen_require.extend(data_keys)
+        return func
+    return require_decorator
+
+
+def will_generate(data_handler, will_generate_keys, **handler_kwargs):
+    if isinstance(will_generate_keys, str):
+        will_generate_keys = (will_generate_keys,)
+    # will_generate_key_set = set(will_generate_keys)
+
+    def will_generate_decorator(func):
+        # pylint: disable=protected-access
+        if hasattr(func, '_feagen_will_generate'):
+            raise NotImplementedError("Multiple will_generate is not supported"
+                                      "yet.")
+        func._feagen_will_generate = {
+            'mode': 'full',
+            'handler': data_handler,
+            'keys': will_generate_keys,
+            'handler_kwargs': handler_kwargs,
+        }
+        return func
+    return will_generate_decorator
+
+
+def will_generate_one_of(data_handler, will_generate_keys, **handler_kwargs):
+    if isinstance(will_generate_keys, str):
+        will_generate_keys = (will_generate_keys,)
+
+    def will_generate_one_of_decorator(func):
+        # pylint: disable=protected-access
+        if hasattr(func, '_feagen_will_generate'):
+            raise NotImplementedError("Multiple will_generate is not supported"
+                                      "yet.")
+        func._feagen_will_generate = {
+            'mode': 'one',
+            'handler': data_handler,
+            'keys': will_generate_keys,
+            'handler_kwargs': handler_kwargs,
+        }
+        return func
+    return will_generate_one_of_decorator
--- feagen/decorators/__init__.py
+++
@@ -1,5 +0,0 @@
-from .data_generation import (
-    require,
-    will_generate,
-    will_generate_one_of,
-)
