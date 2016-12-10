f8090d046fe3618d5b6518c655565a6c7a3d35f8
Ian Lin <andylin514@gmail.com>
2016-12-10 20:59:02 +0800

    involed dag

--- feagen/tools/feagen_runner.py
+++ feagen/tools/feagen_runner.py
@@ -5,11 +5,12 @@ from importlib import import_module
 
 import yaml
 from mkdir_p import mkdir_p
+from feagen.data_generator import draw_dag
 
 from ..bundling import flatten_structure, bundle_data
 
 
-def feagen_run_with_configs(global_config, bundle_config):
+def feagen_run_with_configs(global_config, bundle_config, dag_output_path=None):
     # TODO: check the config
     module_name, class_name = global_config['generator_class'].rsplit(".", 1)
     module = import_module(module_name)
@@ -22,6 +23,8 @@ def feagen_run_with_configs(global_config, bundle_config):
 
     data_keys = flatten_structure(bundle_config['structure'])
     involved_dag = data_generator.generate(data_keys)
+    if dag_output_path is not None:
+        draw_dag(involved_dag, dag_output_path)
 
     mkdir_p(global_config['data_bundles_dir'])
     bundle_path = join(global_config['data_bundles_dir'],
@@ -41,6 +44,8 @@ def feagen_run(argv=sys.argv[1:]):
     parser.add_argument('-b', '--bundle-config',
                         default=".feagenrc/bundle_config.yml",
                         help="the path of the bundle configuration YAML file")
+    parser.add_argument('-d', '--dag-output-path', default=None,
+                        help="draw the involved subDAG to the provided path")
     args = parser.parse_args(argv)
     with open(args.global_config) as fp:
         global_config = yaml.load(fp)
@@ -48,4 +53,4 @@ def feagen_run(argv=sys.argv[1:]):
         bundle_config = yaml.load(fp)
     filename_without_extension = splitext(basename(args.bundle_config))[0]
     bundle_config.setdefault('name', filename_without_extension)
-    feagen_run_with_configs(global_config, bundle_config)
+    feagen_run_with_configs(global_config, bundle_config, args.dag_output_path)
