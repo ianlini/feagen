0ca02a6e499f3d2660ba36cbf03a817ea08474e8
Ian Lin <andylin514@gmail.com>
2016-12-10 20:28:54 +0800

    feature_config -> bundle_config; concat feature -> data bundle

--- feagen/tools/config.py
+++ feagen/tools/config.py
@@ -7,24 +7,23 @@ def init_config():
     mkdir_p(".feagenrc")
     default_global_config = """\
 generator_class: feature_generator.FeatureGenerator
-concat_feature_dir: concat_features
-global_feature_hdf_path: global_feature.h5
+data_bundles_dir: data_bundles
+global_data_hdf_path: global_data.h5
 
-# The additional arguments that will be given when initiating the feature
-# generator object.
+# The additional arguments that will be given when initiating the data generator
+# object.
 generator_kwargs: {}
 """
-    default_feature_config = """\
-# The name of this feature set. This will be the file name of the concatenated
-# data. Another suggested usage is to comment out this line, so the name will be
+    default_bundle_config = """\
+# The name of this bundle. This will be the file name of the data bundle.
+# Another suggested usage is to comment out this line, so the name will be
 # obtained from the file name of this config, that is, the name will be the same
 # as the config file name without the extension.
 name: default
 
-# The structure of the concatenated data. All the involved data will be
-# generated and put into the global feature file first (if not exist),
-# and then be composed according to this structure and then write to the
-# concatenated feature file.
+# The structure of the data bundle. All the involved data will be generated and
+# put into the global data file first (if data not exist), and then be bundled
+# according to this structure, and then write to the data bundle file.
 # Only 'features' in the structure dictionary is required and be treated
 # differently. The value for the key 'features' can only be list of strings, and
 # the features specified in this list will be concatenated to one dataset.
@@ -35,9 +34,9 @@ structure:
   - feature_1
   - feature_2
 
-# The additional arguments that will be given when initiating the feature
-# generator object. If the same key appears in both global config and feature
-# config, the value in the feature config will be used.
+# The additional arguments that will be given when initiating the data generator
+# object. If the same key appears in both global config and bundle config, the
+# value in the bundle config will be used.
 generator_kwargs: {}
 """
     default_global_config_path = join(".feagenrc", "config.yml")
@@ -48,10 +47,10 @@ generator_kwargs: {}
         with open(default_global_config_path, "w") as fp:
             fp.write(default_global_config)
 
-    default_feature_config_path = join(".feagenrc", "feature_config.yml")
-    if exists(default_feature_config_path):
+    default_bundle_config_path = join(".feagenrc", "bundle_config.yml")
+    if exists(default_bundle_config_path):
         print("Warning: %s exists so it's not generated."
-              % default_feature_config_path)
+              % default_bundle_config_path)
     else:
-        with open(default_feature_config_path, "w") as fp:
-            fp.write(default_feature_config)
+        with open(default_bundle_config_path, "w") as fp:
+            fp.write(default_bundle_config)
--- feagen/tools/feagen_runner.py
+++ feagen/tools/feagen_runner.py
@@ -9,42 +9,43 @@ from mkdir_p import mkdir_p
 from ..bundling import flatten_structure, bundle_data
 
 
-def feagen_run_with_configs(global_config, feature_config):
+def feagen_run_with_configs(global_config, bundle_config):
     # TODO: check the config
     module_name, class_name = global_config['generator_class'].rsplit(".", 1)
     module = import_module(module_name)
     generator_class = getattr(module, class_name)
     generator_kwargs = global_config['generator_kwargs']
-    generator_kwargs.update(feature_config['generator_kwargs'])
-    feature_generator = generator_class(
+    generator_kwargs.update(bundle_config['generator_kwargs'])
+    data_generator = generator_class(
         global_data_hdf_path=global_config['global_data_hdf_path'],
         **generator_kwargs)
 
-    data_keys = flatten_structure(feature_config['structure'])
-    involved_dag = feature_generator.generate(data_keys)
+    data_keys = flatten_structure(bundle_config['structure'])
+    involved_dag = data_generator.generate(data_keys)
 
     mkdir_p(global_config['data_bundles_dir'])
     bundle_path = join(global_config['data_bundles_dir'],
-                       feature_config['name'])
-    bundle_data(feature_config['structure'],
+                       bundle_config['name'])
+    bundle_data(bundle_config['structure'],
                 global_data_hdf_path=global_config['global_data_hdf_path'],
                 data_bundle_hdf_path=bundle_path)
 
 
 def feagen_run(argv=sys.argv[1:]):
 
-    parser = argparse.ArgumentParser(description='Generate features.')
+    parser = argparse.ArgumentParser(
+        description="Generate global data and data bundle.")
     parser.add_argument('-g', '--global-config',
                         default=".feagenrc/config.yml",
-                        help="the path of the path configuration yaml file")
-    parser.add_argument('-f', '--feature-config',
-                        default=".feagenrc/feature_config.yml",
-                        help="the path of the feature configuration yaml file")
+                        help="the path of the path configuration YAML file")
+    parser.add_argument('-b', '--bundle-config',
+                        default=".feagenrc/bundle_config.yml",
+                        help="the path of the bundle configuration YAML file")
     args = parser.parse_args(argv)
     with open(args.global_config) as fp:
         global_config = yaml.load(fp)
-    with open(args.feature_config) as fp:
-        feature_config = yaml.load(fp)
-    filename_without_extension = splitext(basename(args.feature_config))[0]
-    feature_config.setdefault('name', filename_without_extension)
-    feagen_run_with_configs(global_config, feature_config)
+    with open(args.bundle_config) as fp:
+        bundle_config = yaml.load(fp)
+    filename_without_extension = splitext(basename(args.bundle_config))[0]
+    bundle_config.setdefault('name', filename_without_extension)
+    feagen_run_with_configs(global_config, bundle_config)
