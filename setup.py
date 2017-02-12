6ff60724cca5131e1d4fec5901f827015a4508bc
Ian Lin <andylin514@gmail.com>
2017-02-12 21:40:46 +0800

    test python 3.6

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: BSD License',
     ],
     test_suite='nose.collector',
